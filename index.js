5e113511b2f510ebacaae823aaf3c177efd6ade2
Catharina <catharina2002@gmail.com>
2024-10-04 10:45:00 +0200

    name-parts

---
+++ index.js
@@ -0,0 +1,5 @@
+const aName = "Peter Heronimous Lind";
+const firstName = aName.substring(0,aName.indexOf(" "))
+/* console.log("_" + firstName + "_");
+ */const middleName = aName.substring(aName.indexOf(" ") + 1,aName.lastIndexOf(" "))
+console.log("_" + middleName + "_");
